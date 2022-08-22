c724ffa2a200ff687e6722978c316adaf24f9514
Frédéric Thériault <fred.theriault@gmail.com>
2022-08-22 14:52:03 -0400

    Done with loops

--- PHP - La base/Notes/02 - boucles.php
+++ PHP - La base/Notes/02 - boucles.php
@@ -6,6 +6,52 @@
 	</head>
 	<body>
 		<h1>Champs radio</h1>
+		<?php
+			for ($i = 0; $i < 10; $i++) {
+				?>
+				<div style='color:red;'>
+					salut <?= $i ?>
+				</div>
+				<?php
+			}
+		?>
+
+		<?php
+			for ($i=0; $i < 10; $i++) { 
+				?>
+				<div>
+					salut <?= $i ?>
+				</div>
+				<?php
+			}
+		?>
+
+		<?php
+			$i = 10;
+
+			while ($i > 0) {
+				$i--;
+				echo $i;
+			}
+		?>
+
+		<?php
+			$tab = []; // array();
+
+			$tab[] = "élément 1"; // comme un push
+			$tab[] = "élément 2"; // comme un push
+
+			// echo $tab;
+			var_dump($tab); // echo pour les structures complexes
+
+			foreach ($tab as $element) {
+				?>
+				<div>
+					<?= $element ?>
+				</div>
+				<?php
+			}
+		?>
 	</body>
 </html>
 
