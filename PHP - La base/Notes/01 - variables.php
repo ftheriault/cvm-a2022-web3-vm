48a398c9b6426dd3c924a7458647ea4563a8adf8
Frédéric Thériault <fred.theriault@gmail.com>
2022-08-22 14:31:31 -0400

    Done with variables

--- PHP - La base/Notes/01 - variables.php
+++ PHP - La base/Notes/01 - variables.php
@@ -1,3 +1,17 @@
+<?php
+	/* */
+	# 
+	// phpt = php tags
+?>
+<?php 
+	$username = false;
+	$username = 0;
+
+	if ($username == "John") {
+		// concaténation = ., pas +
+		$username = $username . " Smith";
+	}
+?>
 <!DOCTYPE html>
 <html lang="fr">
 	<head>
@@ -5,7 +19,16 @@
 		<meta charset="utf-8" />
 	</head>
 	<body>
-		Nom : 
+		<?php 
+			if (1 == 2) {
+				echo "123";
+			}
+			else {
+				echo "456";
+			}
+		?>
+		Nom : <?php echo $username ?><br/>
+		Nom : <?= $username ?> (phpe = php echo)
 	</body>
 </html>
 
