a860d77a72f397cfbe04a66be081477fbb454108
Frédéric Thériault <fred.theriault@gmail.com>
2022-08-22 15:26:43 -0400

    done with forms

--- PHP - La base/Notes/04 - formulaire.php
+++ PHP - La base/Notes/04 - formulaire.php
@@ -1,3 +1,10 @@
+<?php
+	$txt = "";
+
+	if (!empty($_GET["info"])) { // not undefined, not null, not "", not false, not zero
+		$txt = $_GET["info"]; // $_POST["info"]
+	}
+?>
 <!DOCTYPE html>
 <html lang="fr">
 	<head>
@@ -5,9 +12,9 @@
 		<meta charset="utf-8" />
 	</head>
 	<body>
-		<form action="04 - formulaire.php" method="get">
+		<form action="" method="get">
 			<div>
-				Info : <input type="text" name="info" />
+				Info : <input type="text" name="info" value="<?= $txt ?>" />
 			</div>
 			<div>
 				<input type="submit" value="Go!" />
