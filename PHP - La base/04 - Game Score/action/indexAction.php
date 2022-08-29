94f12f604bfeedefe88d5d8ef329a0f3fccb58a1
Frédéric Thériault <fred.theriault@gmail.com>
2022-08-29 15:31:59 -0400

    bug fix

--- PHP - La base/04 - Game Score/action/indexAction.php
+++ PHP - La base/04 - Game Score/action/indexAction.php
@@ -3,6 +3,6 @@
     function execute() {
         if (!empty($_POST["pointage"])) {
             $line = date("Y-m-d H:i:s") . " - " . ($_POST["pointage"]/1000) . " sec.\n";
-            file_put_contents("results.txt", $line);
+            file_put_contents("results.txt", $line, FILE_APPEND);
         }
     }
