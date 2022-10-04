d8ccf30ecbe15fcf6e597657baec47c9667d44b4
Frédéric Thériault <fred.theriault@gmail.com>
2022-10-04 13:01:24 -0400

    Added base AJAX files

---
+++ AJAX - JSON/json.php
@@ -0,0 +1,15 @@
+<?php
+    $tab = array("a", "b", "c");
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+			<title>JSON</title>
+			<meta charset="utf-8">
+	</head>
+	<body>
+		<div>
+
+		</div>
+	</body>
+</html>
